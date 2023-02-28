29f8f3693e669b2f6a3100315cbe40b056ac974d
girlsgotech <120268099+girlsgotech@users.noreply.github.com>
2023-02-28 15:30:29 +0100

    Add files via upload

---
+++ Fom_Validation/main.js
@@ -0,0 +1,40 @@
+/*let username = document.getElementById("username"),
+ email = document.getElementById("email"),
+ password = document.getElementById("password");*/
+
+  /*
+ errorMsg = document.getElementsByClassName("error"),
+ successIcon = document.getElementsByClassName("success-icon"),
+ failureIcon = document.getElementsByClassName("failure-icon")*/
+
+ let id = (id) => document.getElementById(id);
+ let classes = (classes) => document.getElementsByClassName(classes);
+
+ let username = id("username"),
+ email = id("email"),
+ password = id("password")
+ form =id("form"),
+ errorMsg = classes("error"),
+ successIcon = classes("success-icon"),
+ failureIcon = classes("failure-icon"); 
+
+ form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    engine(username, 0, "Username cannot be blank");
+    engine(email, 1, "Email cannot be blank");
+    engine(password, 2, "Password cannot be blank");
+ } );
+
+ let engine = (id, serial, message) => {
+    if (id.value.trim() === ""){
+        errorMsg[serial].innerHTML = message;
+        failureIcon[serial].style.opacity = "1";
+        successIcon[serial].style.opacity = "0";
+    } else {
+        errorMsg[serial].innerHTML = "";
+        failureIcon[serial].style.opacity = "0";
+        successIcon[serial].style.opacity = "1";
+    }
+ };
+
